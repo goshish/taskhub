bd7a6dbdcec9030e832fd2fcff1de2b054aab55b
giorgi <giorgi.oniani.04@gmail.com>
2023-05-14 11:14:12 +0400

    Создал таблицу для задач

--- taskmanager/webapp/models.py
+++ taskmanager/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    priority_choices = [
+        ('low', 'Низкий'),
+        ('medium', 'Средний'),
+        ('high', 'Высокий')
+    ]
+    priority = models.CharField(max_length=10, choices=priority_choices)
+    due_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
