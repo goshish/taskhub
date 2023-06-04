6049940b3de584e42c5796a292bd2de226ef2aea
giorgi <giorgi.oniani.04@gmail.com>
2023-06-04 20:19:37 +0400

    Сделаны представленя через классы

--- taskmanager/webapp/urls.py
+++ taskmanager/webapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-
+from .views import TaskHome, MyDay, Content, Add_task
 
 urlpatterns = [
-    path('', views.tasks, name='tasks'),
-    path('/my_day', views.my_day, name='my_day'),
+    path('', TaskHome.as_view(), name='tasks'),
+    path('/my_day', MyDay.as_view(), name='my_day'),
     path('/list', views.list, name='list'),
     # Для того чтобы переходить в конкретную запись из БД используя шаблонизатор
-    path('/task/<int:task_id>/', views.content, name='content'),
-    path('/add_task', views.add_task, name='add_task')
+    path('/task/<int:task_id>/', Content.as_view(), name='content'),
+    path('/add_task', Add_task.as_view(), name='add_task')
 ]
--- taskmanager/webapp/views.py
+++ taskmanager/webapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import *
 from .models import *
@@ -9,27 +10,35 @@ menu = [{'title': "Мой день", 'url_name': 'my_day'},
         {'title': "Мои списки", 'url_name': 'list'},
         {'title': "Добавить задачу", 'url_name': 'add_task'}
 ]
-def tasks(request):
-    tasks= Task.objects.all()
-    context = {
-        'tasks': tasks,
-        'menu': menu,
-        'title': 'Задачи'
-    }
-    return render(request, 'webapp/tasks.html', context=context)
+
+class TaskHome(ListView):
+    model = Task
+    template_name = 'webapp/tasks.html'
+    context_object_name = 'tasks'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Задачи'
+        return context
 
 
-def my_day(request):
-    tasks = Task.objects.filter(my_day=True)
-    context = {
-        'tasks': tasks,
-        'menu': menu,
-        'title': 'My day'
-    }
-    return render(request, 'webapp/my_day.html', context=context)
+class MyDay(ListView):
+    model = Task
+    template_name = 'webapp/my_day.html'
+    context_object_name = 'tasks'
+    def get_queryset(self):
+        return Task.objects.filter(my_day=1)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Мой день'
+        return context
 
 
 def list(request):
+    tasks = Task.objects.all()
     context = {
         'tasks': tasks,
         'menu': menu,
@@ -38,31 +47,62 @@ def list(request):
     return render(request, 'webapp/list.html', context=context)
 
 
-def content(request, task_id):
-    tasks = Task.objects.filter(id=task_id)
-    context = {
-        'tasks': tasks,
-        'menu': menu,
-        'title': 'Задачи',
+# DetailView
 
-    }
-    return render(request, 'webapp/content.html', context=context)
+class Content(ListView):
+    model = Task
+    template_name = 'webapp/content.html'
+    pk_url_kwarg = 'task_id'
+    context_object_name = 'tasks'
+    def get_queryset(self):
+        task_id = self.kwargs.get('task_id')
+        return Task.objects.filter(id=task_id)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Задачи'
+        return context
+# def content(request, task_id):
+#     tasks = Task.objects.filter(id=task_id)
+#     context = {
+#         'tasks': tasks,
+#         'menu': menu,
+#         'title': 'Задачи',
+#
+#     }
+#     return render(request, 'webapp/content.html', context=context)
+
+# CreateView
+
+class Add_task(CreateView, ListView):
+    model = Task
+    form_class = AddTaskForm
+    template_name = 'webapp/add_task.html'
+    context_object_name = 'tasks'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Задачи'
+        return context
 
 
-def add_task(request):
-    if request.method == 'POST':
-        form = AddTaskForm(request.POST)
-        if form.is_valid():
-                form.save()
-                return redirect('tasks')
-    else:
-        form = AddTaskForm()
-    form = AddTaskForm()
-    tasks = Task.objects.all()
-    context = {
-        'tasks': tasks,
-        'menu': menu,
-        'title': 'Add task',
-        'form': form,
-    }
-    return render(request, 'webapp/add_task.html', context=context)
+
+# def add_task(request):
+#     if request.method == 'POST':
+#         form = AddTaskForm(request.POST)
+#         if form.is_valid():
+#                 form.save()
+#                 return redirect('tasks')
+#     else:
+#         form = AddTaskForm()
+#     form = AddTaskForm()
+#     tasks = Task.objects.all()
+#     context = {
+#         'tasks': tasks,
+#         'menu': menu,
+#         'title': 'Add task',
+#         'form': form,
+#     }
+#     return render(request, 'webapp/add_task.html', context=context)
